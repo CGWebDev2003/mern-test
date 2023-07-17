ebb7be786d9e2388b3efd779ad7c0ea2e6b0f7c0
CGWebDev2003 <developer@colingrahm.de>
2023-07-17 13:27:50 +0200

    Try to post new workout

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/WorkoutModel')
 
 const router = express.Router()
 
@@ -14,7 +15,14 @@ router.get('/:id', (res, req) => {
 
 // POST a new workout
 router.post('/', (req, res) => {
-    
+    const {title, load, reps} = req.body
+
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
     res.json({mssg: 'POST a new Workout'})
 })
 
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express()
 
 // Middleware
 app.use(express.json())
+
 app.use((req, res, next) => {          
     console.log(req.path, req.method)
     next()
